3b726e70c9b102ce0c3647c55e420979f071d7bc
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-09-09 11:23:38 -0300

    feat: Changes loading method to show every image pulled from DataBase

--- app.js
+++ app.js
@@ -1,56 +1,40 @@
 'use strict'
 
-
 const searchButton = document.getElementById('search-button');
+const galeria = document.getElementById('galeria');
 
 //Recebe input do usuário
-function getInput(){ 
+function getInput() {
     const searchBar = document.getElementById('search-bar');
     const input = searchBar.value;
     const searchTerm = input.replace(' ', '/');
     return searchTerm;
 }
 
-//Busca na lista de raças se o input é valido e converte para a estrutura de array
-async function findPath(dogName){
-    const listaEndPoint = 'https://dog.ceo/api/breeds/list/all';
-    const response = await fetch(listaEndPoint);
-    const listaCachorros = await response.json();
-}
-
-
-//Async para deixar uma função assincrona, necessária para usar o await
+//Async para deixar uma função assincrona, necessário quando há uso de await
 //Await é necessário para dar o tempo de fetch, nosso código roda em nanos segundos, mas esse fetch fica na faixa dos milisegundos
-async function buscarImagens(breed){
+async function buscarImagens(breed) {
     const endPoint = `https://dog.ceo/api/breed/${breed}/images`;
     const response = await fetch(endPoint);
     const imagens = await response.json();
-    // console.log(imagens.message);
 
     return imagens.message;
 }
 
+function carregarImagem(sourceImage){
+    const dogImg = document.createElement('img');
+    dogImg.src = sourceImage;
+    galeria.appendChild(dogImg);
+}
 
-async function carregarImagens(){
+
+async function carregarImagens() {
     const input = getInput();
     const sourceImages = await buscarImagens(input);
     console.log(getInput)
-    const galeria = document.getElementById('galeria');
     galeria.replaceChildren()
 
-    for (let i = 0; i< 15 ; i++){ 
-        //TODO: fazer um método próprio para usar forEach, não consegui pensar bem em como limitar a contagem
-        const dogImg = document.createElement('img');
-        dogImg.src = sourceImages[i];
-        // console.log(sourceImages[i]);
-        galeria.appendChild(dogImg);
-        
-    }
+    sourceImages.forEach(carregarImagem);
 }
 
-searchButton.addEventListener('click', carregarImagens)
-
-//Area de testes
-// findPath('shiba');
-// console.log('hello world!')
-
+searchButton.addEventListener('click', carregarImagens);
