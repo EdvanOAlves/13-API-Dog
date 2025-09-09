28f726ef22219a708efadd9f2eb77c5bceef19c0
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-09-09 10:47:46 -0300

    feat: implements search function

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 'use strict'
 
 
+const searchButton = document.getElementById('search-button');
+
+//Recebe input do usuário
+function getInput(){ 
+    const searchBar = document.getElementById('search-bar');
+    const searchTerm = searchBar.value;
+    return searchTerm;
+    // return searchTerm.replace(' ', '-');
+}
+
+//Busca na lista de raças se o input é valido e converte para a estrutura de array
+async function findPath(dogName){
+    const listaEndPoint = 'https://dog.ceo/api/breeds/list/all';
+    const response = await fetch(listaEndPoint);
+    const listaCachorros = await response.json();
+}
+
 
 //Async para deixar uma função assincrona, necessária para usar o await
 //Await é necessário para dar o tempo de fetch, nosso código roda em nanos segundos, mas esse fetch fica na faixa dos milisegundos
@@ -8,11 +25,32 @@ async function buscarImagens(breed){
     const endPoint = `https://dog.ceo/api/breed/${breed}/images`;
     const response = await fetch(endPoint);
     const imagens = await response.json();
-    console.log(imagens.message);
+    // console.log(imagens.message);
 
     return imagens.message;
 }
 
-//Sempre que for solicitar uma função assincrona é necessário colocar o await
-buscarImagens('shiba');
-console.log(imagesArray);
+
+async function carregarImagens(){
+    const input = getInput();
+    const sourceImages = await buscarImagens(input);
+    console.log(getInput)
+    const galeria = document.getElementById('galeria');
+    galeria.replaceChildren()
+
+    for (let i = 0; i< 15 ; i++){ 
+        //TODO: fazer um método próprio para usar forEach, não consegui pensar bem em como limitar a contagem
+        const dogImg = document.createElement('img');
+        dogImg.src = sourceImages[i];
+        // console.log(sourceImages[i]);
+        galeria.appendChild(dogImg);
+        
+    }
+}
+
+searchButton.addEventListener('click', carregarImagens)
+
+//Area de testes
+// findPath('shiba');
+// console.log('hello world!')
+
